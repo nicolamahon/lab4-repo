25f20c2705d102d59721b3ffaa7d906c659f47e7
Nicola Mahon <c15755031@mydit.ie>
2017-10-11 22:27:23 +0000

    working version of app.py, rendering index.html and implementation of app.route/contact-me. All files refactored but content needs to be fleshed out

--- myapp/app.py
+++ myapp/app.py
@@ -1,13 +1,6 @@
 from flask import Flask
 from flask import render_template
 
-
-
-
-#import os
-#import socket
-# Connect to Redis
-#redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = Flask(__name__)
 @app.route("/")
 def statichtml():
@@ -15,12 +8,8 @@ def statichtml():
 
 @app.route("/contact-me")
 def hello():
-    html = ">a href="mailto:c15755031@mydit.ie">Link Text</a>"
-    #html = "<h3>Hello {name}!</h3>" \
-    #       "<b>Hostname:</b> {hostname}<br/>" \
-    #       "<b>Visits:</b> {visits}"
-    #return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    html = "<a href=\"mailto:c15755031@mydit.ie\">Link Text</a>"
     return html
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
